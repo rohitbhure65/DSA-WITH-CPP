5e0cc403a8ba8dd745cbeb159866ba9c27b12996
root <root@kali>
2024-02-11 22:51:45 +0530

    searching technique

--- C++/varibale.cpp
+++ C++/varibale.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream>
+using namespace std;
 
 int main()
 {
@@ -6,10 +8,10 @@ int main()
     unsigned short int x = 34;
     unsigned long int v = 3452343243242;
     unsigned long long int myviews = 8580944534534534530;
-    
-    printf("\t%hd\n", c);
-    printf("\t%hd\n", x);
-    printf("\t%lu\n", v);
-    printf("\t%llu\n", myviews);
+
+    cout << c << endl;
+    cout << x << endl;
+    cout << v << endl;
+    cout << myviews << endl;
     return 0;
 }
