bd96d93a13df0ab1954880378f6a1f68abfa5c53
“Rohit <rohitbhure.cse@gmail.com>
2024-07-13 10:51:30 +0530

    linkedlist deletion operations

--- Linkedlist/lloperations.c
+++ Linkedlist/lloperations.c
@@ -6,12 +6,17 @@ struct node {
     struct node *next;
 };
 
+// Insertion Operations
 // Insert a Node at the Begging of Linked List
 struct node* insertAtHead(struct node **head, int data) {
     struct node *newNode = (struct node *)malloc(sizeof(struct node));
     newNode->data = data;
     newNode->next = *head;
     *head = newNode;
+    // Modification Persistence:
+    // When we pass head as a single pointer (i.e., struct node *head), we are passing a copy of the head pointer. Any changes made to this copy will not affect the original head pointer in the calling function.
+    // By passing head as a pointer to a pointer (i.e., struct node **head), we pass the address of the head pointer. This allows the function to modify the original head pointer itself, not just a copy of it.
+    printf("Insert a Node at the Begging of Linked List\n");
 }
 
 // Insert a Node at The Tail of the Linked List
@@ -29,6 +34,7 @@ struct node* insertAtEnd(struct node **head, int data) {
         }
         cur->next = newNode;
     }
+    printf("Insert a Node at The Tail of the Linked List\n");
 }
        
 // Insert a Node at the Given index On the Linked List
@@ -53,10 +59,41 @@ struct node* inserAtindex(struct node **head,int data, int index){
             cur->next = newNode;
         }
     }
+    printf("Insert a Node at the Given index On the Linked List\n");
 }
 
+// Deletion Operations
+// Deleting The first Node in Singly Linked List
+void *deletefirst(struct node **head){
+    struct node *temp;
+    if(*head==NULL)
+        return *head;
+    temp = *head;
+    *head = (*head)->next;
+    free(temp);
+    
+    printf("Deleting The first Node in Singly Linked List\n");
+};
+
+// Deleting the last Node in Singly Linked List
+void *deletelast(struct node **head){
+    struct node *temp=NULL, *curr=*head;
+    if(*head==NULL){
+        return *head;
+    }
+    while (curr->next)
+    {
+        temp=curr;
+        curr=curr->next;
+    }
+    temp->next=NULL;
+    free(curr);
+    
+    printf("Deleting the last Node in Singly Linked List\n");
+} 
+
 // Traversing of the Linked List
-void display(struct node *head) {
+struct node* display(struct node *head) {
     struct node *temp = head;
     while (temp != NULL) {
         printf("%d->", temp->data);
@@ -78,6 +115,12 @@ int main() {
     display(head);
 
     inserAtindex(&head,4,5);
+    display(head);
+
+    deletefirst(&head);
+    display(head);
+
+    deletelast(&head);
     display(head);
     return 0;
 }
