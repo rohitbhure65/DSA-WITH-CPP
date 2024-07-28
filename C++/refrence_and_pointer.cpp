ee4410d800c88cdf2a25c139b30f570e8aea410a
Rohit Bhure <rohitbhure.cse@gmail.com>
2024-07-28 23:24:02 +0530

    debug swap function using pointer

--- C++/refrence_and_pointer.cpp
+++ C++/refrence_and_pointer.cpp
@@ -6,27 +6,30 @@ void swap_value(int a, int b){
     int temp = a;
     a = b;
     b = temp;
-}
+};
 
 // call by reference
 void swap_reference(int &a, int &b){
     int temp = a;
     a = b;
     b = temp;
-}
+};
 
 // function with pointer
-void swap_pointer(int *a, int *b){
+void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
     *b = temp;
-}
+};
 
 int main(){
     int a = 10;
     int b = 20;
+
+    int x = 40;
+    int y = 80;
     swap_value(a, b);     // call by value
     swap_reference(a, b); // call by reference
-    swap_pointer(&a, &b); // function with pointer
+    swap(&x, &y);         // function with pointer
     return 0;
 }
